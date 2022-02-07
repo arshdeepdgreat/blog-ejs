f480777010dc64d5fb9166c1a0e08b54e3fc203c
arshdeepdgreat <arshdeepdgreat@gmail.com>
2022-02-07 11:21:39 +0530

    commit

--- app.js
+++ app.js
@@ -22,8 +22,16 @@ app.get("/",function(req,res){
 app.get("/about",function(req,res){
   res.render("about" , {startingContent : aboutContent});
 })
+
 app.get("/contact",function(req,res){
   res.render("contact" , {startingContent : contactContent});
+})
+
+app.get("/compose",function(req,res){
+  res.render("compose");
+})
+app.post("/compose",function(req,res){
+  console.log(req.body.compose);
 })
 
 
